1aaf9fe1b36d51c594c2278adfdc0296b107adea
Pat <mediodia.patrick@gmail.com>
2024-03-28 13:19:23 +0800

    added viewing of user object

--- sample-posts-auth/src/components/ViewProfile.jsx
+++ sample-posts-auth/src/components/ViewProfile.jsx
@@ -1,7 +1,10 @@
 import { Link } from 'react-router-dom';
 import userpool from '../services/userpool';
+import { useState } from 'react';
 
 export default function ViewProfile() {
+    const [viewUserObject, setViewUserObject] = useState(false);
+
     const currentUser = userpool.getCurrentUser();
     currentUser.getSession((err, res)=> {
         if (err) reject(err);
@@ -15,86 +18,92 @@ export default function ViewProfile() {
         given_name, 
         middle_name 
     } = currentUser.signInUserSession.idToken.payload;
-
-    /*
-    <div className="pre-body">
-        <pre> { JSON.stringify(currentUser, null, 2) } </pre>
-    </div>
-    */
-
-    return (
-        <div className="form">
-            <h1 className="form-header">Profile</h1>
-            <div className="form-field">
-                Email: 
-                <input
-                    className="form-input"
-                    name='email'
-                    type='text'
-                    value={email}
-                    autoComplete="off"
-                    disabled
-                />
-            </div>
-            <div className="form-field">
-                First Name: 
-                <input
-                    className="form-input"
-                    name='given_name'
-                    type='text'
-                    value={given_name}
-                    autoComplete="off"
-                    disabled
-                />
-            </div>
-            <div className="form-field">
-                Middle Name: 
-                <input
-                    className="form-input"
-                    name='middle_name'
-                    type='text'
-                    value={middle_name}
-                    autoComplete="off"
-                    disabled
-                />
-            </div>
-            <div className="form-field">
-                Last Name: 
-                <input
-                    className="form-input"
-                    name='family_name'
-                    type='text'
-                    value={family_name}
-                    autoComplete="off"
-                    disabled
-                />
-            </div>
-            <div className="form-field">
-                Phone Number: 
-                <input
-                    className="form-input"
-                    name='phone_number'
-                    type='text'
-                    value={phone_number}
-                    autoComplete="off"
-                    disabled
-                />
-            </div>
-            <div className="form-field">
-                Address 
-                <input
-                    className="form-input"
-                    name='address'
-                    type='text'
-                    value={address.formatted}
-                    autoComplete="off"
-                    disabled
-                />
-            </div>
-            <div className="form-link">
-                <Link to='/changepassword' className="form-link">Edit Profile | </Link>
-                <Link to='/changepassword' className="form-link">Change Password</Link>
-            </div>
+    
+    const userObject = <div className="pre-body">
+        <h1 className="form-header">User Object</h1>
+        <div className="form-link">
+            <Link className="form-link" onClick={()=>setViewUserObject(false)}>View Profile</Link>
         </div>
-    );
+        <pre>{JSON.stringify(currentUser, null, 2)}</pre>
+    </div>;
+
+    const profile =  <div className="form">
+        <h1 className="form-header">Profile</h1>
+        <div className="form-field">
+            Email: 
+            <input
+                className="form-input"
+                name='email'
+                type='text'
+                value={email}
+                autoComplete="off"
+                disabled
+            />
+        </div>
+        <div className="form-field">
+            First Name: 
+            <input
+                className="form-input"
+                name='given_name'
+                type='text'
+                value={given_name}
+                autoComplete="off"
+                disabled
+            />
+        </div>
+        <div className="form-field">
+            Middle Name: 
+            <input
+                className="form-input"
+                name='middle_name'
+                type='text'
+                value={middle_name}
+                autoComplete="off"
+                disabled
+            />
+        </div>
+        <div className="form-field">
+            Last Name: 
+            <input
+                className="form-input"
+                name='family_name'
+                type='text'
+                value={family_name}
+                autoComplete="off"
+                disabled
+            />
+        </div>
+        <div className="form-field">
+            Phone Number: 
+            <input
+                className="form-input"
+                name='phone_number'
+                type='text'
+                value={phone_number}
+                autoComplete="off"
+                disabled
+            />
+        </div>
+        <div className="form-field">
+            Address 
+            <input
+                className="form-input"
+                name='address'
+                type='text'
+                value={address.formatted}
+                autoComplete="off"
+                disabled
+            />
+        </div>
+        <div className="form-link">
+            <Link to='/changepassword' className="form-link">Edit Profile</Link>
+            &nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+            <Link to='/changepassword' className="form-link">Change Password</Link>
+        </div>
+        <div className="form-link">
+            <Link className="form-link" onClick={()=>setViewUserObject(true)}>View User Object</Link>
+        </div>
+    </div>;
+
+    return (viewUserObject ? userObject : profile);
 }
